26e3ec9f045f4a3c415b96a04b10de94cbf5799a
whqet <whqet@qq.com>
2016-03-28 22:47:33 +0800

    modify editor.html

--- ppt/demos/lib/htmleditor.js
+++ ppt/demos/lib/htmleditor.js
@@ -1,78 +1,66 @@
-(function() {
-    'use strict';
+var emmetSupport=true,
+    texteditor=$("#editor"),
+    previewer=$("#preview"),
+    fileParam = getUrlParam('file'),
+    codeMirror;
 
-    //分隔窗口
-    document.title = 'html在线编辑器';
-    var pane = $('div.split-pane').children('.split-pane-divider');
-    $('div.split-pane').splitPane();
+//分隔窗口
+$('div.split-pane').splitPane();
 
-    //CodeMirror实现编辑和预览
-    function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-        if (r != null) return unescape(r[2]); return null; //返回参数值
-    }
-    
-    var texteditor = document.querySelector('textarea');
-    
-    /*
-    var file=getUrlParam('file'),
-        url= window.location.href,
-        fileUrl = url.substr(0,url.lastIndexOf('/')+1),
-        reader = new FileReader();
+//CodeMirror实现编辑和预览
+//获取url地址的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
+}
+function createScript(jsUrl){
+  var body = document.getElementsByTagName('body')[0];
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = jsUrl;
+  body.appendChild(script);
+}
 
-    if(file){
-        reader.readAsText(fileUrl+file+'.html');
-        reader.addEventListener('load', function() {
-            texteditor.value = this.result;
-        });  
-    }else{
-        texteditor.value = [
-            '<!DOCTYPE html>',
-            '<html>',
-            '<head>',
-            '    <meta charset="utf-8" />',
-            '    <title></title>',
-            '</head>',
-            '<body>',
-            '',
-            '</body>',
-            '</html>',
-        ].join('\n');
-    }
-    */
-
-    texteditor.value = [
-        '<!DOCTYPE html>',
-        '<html>',
-        '<head>',
-        '    <meta charset="utf-8" />',
-        '    <title></title>',
-        '</head>',
-        '<body>',
-        '',
-        '</body>',
-        '</html>',
-    ].join('\n');
-
-    var ed = CodeMirror.fromTextArea(texteditor, {
+function initEditor(){
+    codeMirror = CodeMirror.fromTextArea(texteditor[0], {
         lineNumbers: true,
         mode: 'htmlmixed',
         viewportMargin: Infinity,
         theme: 'monokai'
     });
-
-    emmetCodeMirror(ed);
-
-    function setPreview() {
-        // document.querySelector('#preview').innerHTML = ed.getValue(); // innerHTML won't execute scripts, so we use jQuery
-        requestAnimationFrame(function() {
-            // $('#preview').html(ed.getValue());
-            $('#preview')[0].srcdoc = ed.getValue();
-        });
+    if (emmetSupport) {
+        createScript("lib/emmet.js");
+        emmetCodeMirror(codeMirror);
     }
-
+    codeMirror.on('change', setPreview);
     setPreview();
-    ed.on('change', setPreview);
+}
 
-}());
+function setPreview() {
+    requestAnimationFrame(function() {
+        previewer[0].srcdoc = codeMirror.getValue();
+    });
+}
+
+function getEditorData(){
+    if (fileParam) {
+        var url = window.location.href,
+            fileUrl = url.substr(0, url.lastIndexOf('/') + 1) + fileParam + '.html';
+
+        $.ajax({
+            url: fileUrl,
+            success: function(data) {
+                texteditor[0].value = data;
+                initEditor();
+            }
+        });
+
+    } else {
+        texteditor[0].value = "!";
+        initEditor();
+    }
+}
+
+getEditorData();
